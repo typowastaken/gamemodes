0d6631c9d0c5033948f9869bc7e39ca7653412cc
typowastaken <dogslovetheworld@outlook.com>
2024-08-14 15:50:36 -0230

    #Planned Public Release 1.0.1#
    + Metrics via bStats (https://bstats.org/)
    + Sound effects
    + Sound effects toggle
    + Config file
    + Small bug fixes

--- src/main/java/com/typocreates/gamemodes/Gamemodes.java
+++ src/main/java/com/typocreates/gamemodes/Gamemodes.java
@@ -4,7 +4,12 @@ import com.typocreates.gamemodes.commands.GmaCommand;
 import com.typocreates.gamemodes.commands.GmcCommand;
 import com.typocreates.gamemodes.commands.GmsCommand;
 import com.typocreates.gamemodes.commands.GmspCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 //Created with love by Typo <3
 
@@ -14,6 +19,30 @@ public final class Gamemodes extends JavaPlugin {
     public void onEnable() {
         // Plugin startup login
         plugin = this;
+
+
+
+        Metrics metrics = new Metrics(this, 23009);
+        metrics.addCustomChart(new Metrics.SingleLineChart("players", () -> Bukkit.getOnlinePlayers().size()));
+
+        metrics.addCustomChart(new Metrics.DrilldownPie("java_version", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+            String javaVersion = System.getProperty("java.version");
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(javaVersion, 1);
+            if (javaVersion.startsWith("1.7")) {
+                map.put("Java 1.7", entry);
+            } else if (javaVersion.startsWith("1.8")) {
+                map.put("Java 1.8", entry);
+            } else if (javaVersion.startsWith("1.9")) {
+                map.put("Java 1.9", entry);
+            } else {
+                map.put("Other", entry);
+            }
+            return map;
+        }));
+
+
         saveDefaultConfig();
         saveConfig();
         getCommand("gma").setExecutor(new GmaCommand());
