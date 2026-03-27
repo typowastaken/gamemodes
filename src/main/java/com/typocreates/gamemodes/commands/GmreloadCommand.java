98b6d70e0cc1c8ac6773a120a376aef06a59e533
typowastaken <dogslovetheworld@outlook.com>
2026-03-27 21:16:58 -0230

    Create GmreloadCommand file with plugin getter

---
+++ src/main/java/com/typocreates/gamemodes/commands/GmreloadCommand.java
@@ -0,0 +1,17 @@
+package com.typocreates.gamemodes.commands;
+
+import com.typocreates.gamemodes.Gamemodes;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GmreloadCommand implements CommandExecutor {
+    private final Gamemodes plugin;
+    GmreloadCommand(Gamemodes plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    }
+}
