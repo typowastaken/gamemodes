6716e64e3fa8d01af67d495594cb32a00dd6dc98
typowastaken <dogslovetheworld@outlook.com>
2026-03-27 21:33:08 -0230

    Finished adding reload command & updated config wording slightly to be more accurate

--- src/main/java/com/typocreates/gamemodes/commands/GmreloadCommand.java
+++ src/main/java/com/typocreates/gamemodes/commands/GmreloadCommand.java
@@ -1,19 +1,24 @@
 package com.typocreates.gamemodes.commands;
 
 import com.typocreates.gamemodes.Gamemodes;
+import com.typocreates.gamemodes.utils.GeneralUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class GmreloadCommand implements CommandExecutor {
     private final Gamemodes plugin;
-    public GmreloadCommand(Gamemodes plugin) {
+    private final GeneralUtil gu;
+    public GmreloadCommand(Gamemodes plugin, GeneralUtil gu) {
         this.plugin = plugin;
+        this.gu = gu;
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         plugin.reloadConfig();
+
+        gu.sendMessage(commandSender, "Config reloaded!");
         return true;
     }
 }
