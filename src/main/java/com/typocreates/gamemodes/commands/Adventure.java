adb4faebd6ec5dbed2338fa11ac21304709e9d71
typowastaken <78181633+typowastaken@users.noreply.github.com>
2026-09-20 22:05:33 -0230

    Use a GM enum in the various gamemode commands

--- src/main/java/com/typocreates/gamemodes/commands/Adventure.java
+++ src/main/java/com/typocreates/gamemodes/commands/Adventure.java
@@ -1,5 +1,6 @@
 package com.typocreates.gamemodes.commands;
 import com.typocreates.gamemodes.data.GmLockData;
+import com.typocreates.gamemodes.model.GM;
 import com.typocreates.gamemodes.utils.GeneralUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
@@ -19,7 +20,8 @@ public class Adventure implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NonNull CommandSender commandSender, @NonNull Command command, @NonNull String s, String[] strings) {
-        String gamemode = "Adventure";
+        String gamemodeLabel = GM.ADVENTURE.label;
+        GameMode gamemode = GM.ADVENTURE.gamemode;
 //        If there are no args, set players gamemode, if the commandSender isn't a player, send error.
         if (strings.length == 0) {
             if (commandSender instanceof Player player) {
@@ -27,12 +29,12 @@ public class Adventure implements CommandExecutor {
                     gu.sendErrorMessage(player, gu.getGamemodeLockedMsg());
                     return true;
                 }
-                if (gu.isGamemodeBlocked(player, GameMode.ADVENTURE)) {
-                    gu.sendErrorMessage(commandSender, gu.getGamemodeBlockedMsg(gamemode));
+                if (gu.isGamemodeBlocked(player, gamemode)) {
+                    gu.sendErrorMessage(commandSender, gu.getGamemodeBlockedMsg(gamemodeLabel));
                     return true;
                 }
-                gu.sendMessage(player, gu.getTargetGamemodeChangeMsg(gamemode));
-                player.setGameMode(GameMode.ADVENTURE);
+                gu.sendMessage(player, gu.getTargetGamemodeChangeMsg(gamemodeLabel));
+                player.setGameMode(gamemode);
                 return true;
             }
             gu.sendMessage(commandSender, gu.getExecutorNotPlayerMsg());
@@ -50,14 +52,14 @@ public class Adventure implements CommandExecutor {
                 gu.sendErrorMessage(commandSender, gu.getGamemodeLockedMsg());
                 return true;
             }
-            if (gu.isGamemodeBlocked(target, GameMode.ADVENTURE)) {
-                gu.sendErrorMessage(commandSender, gu.getGamemodeBlockedMsg(gamemode));
+            if (gu.isGamemodeBlocked(target, gamemode)) {
+                gu.sendErrorMessage(commandSender, gu.getGamemodeBlockedMsg(gamemodeLabel));
                 return true;
             }
-            target.setGameMode(GameMode.ADVENTURE);
-            gu.sendMessage(commandSender, gu.getExecutorConfirmationMsg(gamemode, target.getName()));
+            target.setGameMode(gamemode);
+            gu.sendMessage(commandSender, gu.getExecutorConfirmationMsg(gamemodeLabel, target.getName()));
             if (gu.sendTarget() && commandSender != target) {
-                gu.sendMessage(target, gu.getTargetGamemodeChangeMsg(gamemode));
+                gu.sendMessage(target, gu.getTargetGamemodeChangeMsg(gamemodeLabel));
             }
             return true;
         }
